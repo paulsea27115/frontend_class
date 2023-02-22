bf7f309b2d2fe0b48eb001b7a39800d6d46b4826
paulsea27115 <csb070115@gmail.com>
2023-02-22 10:08:13 +0900

    직접 선택자 연습

---
+++ 3-day1/app.js
@@ -0,0 +1,13 @@
+// 전체
+// $("*").css("background","grey");
+
+// 아이디
+// 체이닝 기법 : 선택한 요소에 대한 메서드를 연속해서 사용. ";"로 끝내기
+// $("#queen").css("background","blue").css("color","white");
+
+// 클래스
+// $(".chess").css("color","blue");
+
+// 종속 선택자 -> 선택한 요소의 id/class 값이 일치하는 요소
+// "Tag.class" / "Tag .class" 띄어쓰기 X
+// $("h2.queen").css("color","orange");
